073cb7a1c4930384306be4d94174dca96b2b3490
taybreutz <taybreutz@gmail.com>
2021-09-25 16:39:41 -0400

    drone OOP start

---
+++ classes.py
@@ -0,0 +1,26 @@
+class Drone:
+    def __init__(self, nickname, model, battery_percentage):
+        self.nickname = nickname
+        self.model = model
+        self.battery_percentage = battery_percentage
+
+    def show_stats(self):
+        print(f"Hello Officer Randy, my battery is currently at {self.battery_percentage}")
+
+    def hover_above_home(self):
+        print("hovering above home base")
+
+    def hover_above_mobile(self):
+        print("hovering above mobile officer")
+
+    def move_to_vehicle_in_question(self):
+        print("moving to vehicle")
+
+    def return_to_officer_vehicle(self):
+        print("returning to officer: safe")
+
+
+Drone_1 = Drone("taytay", "s1919", "100%")
+print(Drone_1)
+
+
